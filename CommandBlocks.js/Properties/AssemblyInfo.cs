8ecc1556ee9a2d666ce50fd12300c9dc50f230b4
M4GNV5 <eragonjml@googlemail.com>
2014-08-31 16:40:08 +0200

    updated AssemblyInfo.cs

--- CommandBlocks.js/Properties/AssemblyInfo.cs
+++ CommandBlocks.js/Properties/AssemblyInfo.cs
@@ -4,13 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle("CommandBlocks.js")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Agent J")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("M4GNV5")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
