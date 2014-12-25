e43821c170e4eff23b6d2db36c230912ea27a832
M4GNV5 <eragonjml@googlemail.com>
2014-12-25 20:11:00 +0100

    rewrote runtimeInteger & removed runtimeByte

--- Core/runtimeVariables.js
+++ Core/runtimeVariables.js
@@ -1,55 +1,93 @@
-function RuntimeByte()
+function RuntimeInteger(options)
 {
-	if (!RuntimeByte.nextPosition)
+	if(typeof RuntimeInteger.score == 'undefined')
 	{
-		RuntimeByte.nextPosition = startPosition.clone();
-		RuntimeByte.nextPosition.x--;
+		RuntimeInteger.score = new Score("std.values", "dummy");
 	}
 
-	this.position = RuntimeByte.nextPosition.clone();
-	RuntimeByte.nextPosition.z++;
+	options = options || {};
+
+	this.name = options.name || Naming.next("int");
+
+	options.startValue = options.startValue || 0;
+	RuntimeInteger.score.set(this.name, options.startValue);
+
 
 	this.set = function(value)
 	{
-		var tilename = TileName.byId(value);
-		command('setblock ' + this.position + ' ' + tilename + ' 0');
+		RuntimeInteger.score.set(this.name, value);
 	}
-	this.setTo = function(otherByte)
+	this.add = function(value)
 	{
-		var otherPos = otherByte.position;
-
-		command('clone ' + otherPos + ' ' + otherPos + ' ' + this.position);
+		RuntimeInteger.score.add(this.name, value);
 	}
+	this.remove = function(value)
+	{
+		RuntimeInteger.score.remove(this.name, value);
+	}
+	this.reset = function()
+	{
+		RuntimeInteger.score.reset(this.name);
+	}
+
+	this.test = function(callback, min, max)
+	{
+		RuntimeInteger.score.test(this.name, callback, min, max);
+	}
+	this.operation = function(operation, other, otherPlayer)
+	{
+		RuntimeInteger.score.operation(this.name, operation, otherPlayer, other);
+	}
+
 	this.hasValue = function(value, callback)
 	{
-		var tilename = TileName.byId(value);
-		var command = 'testforblock ' + this.position + ' ' + tilename;
+		return this.isBetween(value, value, callback);
+	}
+	this.isBetween = function(min, max, callback)
+	{
+		var command ="scoreboard players test "+this.name+" "+RuntimeInteger.score.name+" "+min+" "+max;
 
-		if (typeof callback != 'undefined')
-		{
+		if(typeof callback !== 'undefined')
 			validate(command, callback);
-		}
 
 		return command;
 	}
+
+	this.asTellrawExtra = function()
+	{
+		var extra =
+		{
+			obj:
+			{
+				score:
+				{
+					name: "",
+					objective: ""
+				}
+			}
+		};
+		extra.obj.score.name = this.name;
+		extra.obj.score.objective = RuntimeInteger.score.name;
+
+		return extra;
+	}
 }
-RuntimeByte.nextPosition = false;
 
 function RuntimeBoolean()
 {
-	this.base = new RuntimeByte();
+	this.base = new RuntimeInteger();
 
 	this.set = function(value)
 	{
-		if (value)
-			this.base.set(7);
+		if(value)
+			this.base.set(1);
 		else
 			this.base.set(0);
 	}
 	this.hasValue = function(value, callback)
 	{
-		if (value)
-			return this.base.hasValue(7, callback);
+		if(value)
+			return this.base.hasValue(1, callback);
 		else
 			return this.base.hasValue(0, callback);
 	}
@@ -63,154 +101,43 @@ function RuntimeBoolean()
 		return this.hasValue(false, callback);
 	}
 
-	this.switch = function(isTrue, isFalse)
-	{
-		var bool = this;
-		delay();
-		sidewards(function()
-		{
-			queryCommand(bool.hasValue(7), false);
-			comparator();
-			call(isTrue, false);
-		});
-		delay();
-		sidewards(function()
-		{
-			command("setblock ~-1 ~ ~2 minecraft:unpowered_repeater 1", false);
-			delay();
-			delay();
-			call(isFalse, false);
-		});
-	}
-}
-
-function RuntimeInteger(options)
-{
-	if (!RuntimeInteger.mobSpawned)
-	{
-		callOnce(function()
-		{
-			command('summon Chicken ~ ~1 ~ {CustomName:"' + RuntimeInteger.mobName + '",NoAI:true}');
-		});
-		delay(2);
-
-		RuntimeInteger.mobSpawned = true;
-	}
-
-	options = options || {};
-
-	options.name = options.name || Naming.next("int");
-
-	var score = new Score(options.name, "dummy");
-
-	if (typeof options.startValue != 'undefined' && options.startValue !== false)
-		score.set(RuntimeInteger.mob, options.startValue);
-
-	this.name = options.name;
-
-	this.set = function(value)
-	{
-		score.set(RuntimeInteger.mob, value);
-	}
-	this.add = function(value)
-	{
-		score.add(RuntimeInteger.mob, value);
-	}
-	this.remove = function(value)
-	{
-		score.remove(RuntimeInteger.mob, value);
-	}
-	this.reset = function()
-	{
-		score.reset(RuntimeInteger.mob);
-	}
-
-	this.test = function(callback, min, max)
-	{
-		score.test(RuntimeInteger.mob, callback, min, max);
-	}
-	this.operation = function(operation, other, otherPlayer)
-	{
-		otherPlayer = otherPlayer || RuntimeInteger.mob;
-		score.operation(RuntimeInteger.mob, operation, otherPlayer, other);
-	}
-
-	this.hasValue = function(value, callback)
-	{
-		return this.isBetween(value, value, callback);
-	}
-	this.isBetween = function(min, max, callback)
-	{
-		var attributes = {};
-		attributes["name"] = "intmob";
-		attributes["score_" + options.name + "_min"] = min.toString();
-		if (typeof max != 'undefined' && max !== false)
-			attributes["score_" + options.name] = max.toString();
-		var selector = new Selector("e", attributes);
-
-		if (typeof callback != 'undefined')
-		{
-			testfor(selector, callback);
-		}
-
-		return selector;
-	}
-
 	this.asTellrawExtra = function()
 	{
-		//Minecraft Bug https://bugs.mojang.com/browse/MC-74002
-		//workaround giving value to all player
-		score.operation("@a", "=", RuntimeInteger.mob, options.name);
-		var extra =
-		{
-			obj:
-			{
-				score:
-				{
-					name: "@r",
-					objective: ""
-				}
-			}
-		};
-		extra.obj.score.objective = options.name;
-		return extra;
+		return this.base.asTellrawExtra();
 	}
 }
-RuntimeInteger.mobSpawned = false;
-RuntimeInteger.mobName = 'intmob'
-RuntimeInteger.mob = '@e[name=' + RuntimeInteger.mobName + ']';
 
 function RuntimeString(value)
 {
-	if (!RuntimeString.lastIndex || !RuntimeString.indexScore)
+	if(!RuntimeString.lastIndex || !RuntimeString.indexScore)
 	{
 		RuntimeString.lastIndex = 0;
 		RuntimeString.indexScore = new Score("strings", "dummy");
 	}
 
 	RuntimeString.lastIndex++;
-	this.selector = Selector.parse("@e[score_strings_min=" + RuntimeString.lastIndex + ",score_strings=" + RuntimeString.lastIndex + "]");
+	this.selector = Selector.parse("@e[score_strings_min="+RuntimeString.lastIndex+",score_strings="+RuntimeString.lastIndex+"]");
 
 	value = value || Naming.next("string");
 
 	callOnce(function()
 	{
-		command('summon Chicken ~ ~1 ~ {CustomName:"' + value + '",NoAI:true}');
+		command('summon Chicken ~ ~1 ~ {CustomName:"'+value+'",NoAI:true}');
 
-		RuntimeString.indexScore.set('@e[name=' + value + ']', RuntimeString.lastIndex);
+		RuntimeString.indexScore.set('@e[name='+value+']', RuntimeString.lastIndex);
 	});
 	delay(4);
 
 	this.set = function(value)
 	{
-		command('entitydata ' + this.selector + ' {CustomName:"' + value + '"}');
+		command('entitydata '+this.selector+' {CustomName:"'+value+'"}');
 	}
 	this.hasValue = function(value, callback)
 	{
 		var hasValueSelector = this.selector.clone();
 		hasValueSelector.setAttribute("name", value);
 
-		if (typeof callback != 'undefined')
+		if(typeof callback != 'undefined')
 		{
 			testfor(hasValueSelector, callback);
 		}
