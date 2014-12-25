0304b1f2ccece2e34485da4e464fa169a15e635e
M4GNV5 <eragonjml@googlemail.com>
2014-12-25 23:55:57 +0100

    Cmd.GUI -> GUI

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Cmd.GUI
+namespace CommandBlocksJS.GUI
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CommandBlocksJS.Cmd.GUI
+namespace CommandBlocksJS.GUI
 {
 	public class TextBoxStreamWriter : TextWriter
 	{
