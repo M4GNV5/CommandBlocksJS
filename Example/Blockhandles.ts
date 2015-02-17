94ec2984b8d53342c2a9ca4244cb183510b95a30
Jakob Löw <eragonjml@googlemail.com>
2015-02-17 17:55:11 +0100

    fancy 2D landscap <3
    * updated Blockhandles to place a 2D ladscape image

--- Example/Blockhandles.ts
+++ Example/Blockhandles.ts
@@ -1,20 +1,29 @@
 /// <reference path="../Core/API.ts"/>
 
-var block_screen = new Array<Block.BlockHandle>();
-for (var x = 0; x < 16; x++)
+var handle = new Block.BlockHandle(new Util.Vector3(0, 8, 0), "wool", 0);
+
+function fill(x, y, w, h, color)
 {
-	for (var y = 0; y < 9; y++)
-	{
-		// Add every handle for easier setting of blocks
-		var b = new Block.BlockHandle(x, 8 + y, 0, "wool", Block.WoolColor.black);
-		block_screen.push(b);
-	}
+	handle.data = color;
+	handle.fill(x, 8 + y, 0, x + w - 1, 8 + y + h - 1, 0);
 }
 
-var handle = new Block.BlockHandle(new Util.Vector3(0, 8, 0), "wool", Block.WoolColor.black);
-handle.fill(0, 8, 0, 15, 16, 0);
+fill(0, 0, 16, 9, Block.WoolColor.cyan);
 
-function getHandle(x: number, y: number): Block.BlockHandle
-{
-	return block_screen[x + y * 16];
-}
+fill(0, 2, 2, 1, Block.WoolColor.green);
+fill(1, 3, 7, 1, Block.WoolColor.green);
+fill(7, 2, 4, 1, Block.WoolColor.green);
+fill(10, 1, 6, 1, Block.WoolColor.green);
+fill(2, 6, 3, 2, Block.WoolColor.green);
+fill(9, 5, 3, 2, Block.WoolColor.green);
+fill(3, 8, 1, 1, Block.WoolColor.green);
+fill(10, 7, 1, 1, Block.WoolColor.green);
+
+fill(0, 0, 16, 1, Block.WoolColor.brown);
+fill(0, 1, 10, 1, Block.WoolColor.brown);
+fill(2, 2, 5, 1, Block.WoolColor.brown);
+fill(3, 4, 1, 2, Block.WoolColor.brown);
+fill(10, 3, 1, 2, Block.WoolColor.brown);
+
+fill(14, 6, 2, 1, Block.WoolColor.yellow);
+fill(13, 7, 3, 2, Block.WoolColor.yellow);
