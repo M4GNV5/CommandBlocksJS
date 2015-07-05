3b3489e0d050de9ca9697a36b71165be321e320a
Jakob Löw <eragonjml@googlemail.com>
2015-07-05 23:00:31 +0200

    updated MinecraftCommand.validate
    - fixed bug when first argument was undefined but second one not
    - added useSetblockOption

--- Core/MinecraftCommand.ts
+++ Core/MinecraftCommand.ts
@@ -14,7 +14,7 @@ class MinecraftCommand
 		command(this.cmd);
 	}
 
-	validate(callback?: Function, otherwise?: Function): void
+	validate(callback?: Function, otherwise?: Function, useSetblock: boolean = false): void
 	{
 		usedLibs["integer"] = true;
 		usedLibs["setTimeout"] = true;
@@ -30,13 +30,26 @@ class MinecraftCommand
 		if (typeof callback != 'undefined')
 		{
 			var id = outputHandler.addFunction(callback);
-			var _cmd = 'execute @e[name={0},score_stdInteger_min=1] ~4 ~-1 ~ summon ArmorStand ~%X ~%Y ~%Z {CustomName:"call",NoGravity:true,Invincible:true,PersistenceRequired:true}'.format(name);
+
+			var _cmd: string;
+			if (useSetblock)
+				_cmd = "execute @e[name={0},score_stdInteger_min=1] ~4 ~-1 ~ setblock ~%X ~%Y ~%Z minecraft:redstone_block 0 replace".format(name);
+			else
+				var _cmd = 'execute @e[name={0},score_stdInteger_min=1] ~4 ~-1 ~ summon ArmorStand ~%X ~%Y ~%Z {CustomName:"call",NoGravity:true,Invincible:true,PersistenceRequired:true}'.format(name);
+
 			outputHandler.addToCurrent(new Output.FunctionCall(id, _cmd));
 		}
 		if (typeof otherwise != 'undefined')
 		{
 			var id = outputHandler.addFunction(otherwise);
-			var _cmd = 'execute @e[name={0},score_stdInteger=0] ~5 ~-1 ~ summon ArmorStand ~%X ~%Y ~%Z {CustomName:"call",NoGravity:true,Invincible:true,PersistenceRequired:true}'.format(name);
+			var distance = typeof callback != 'undefined' ? 5 : 4;
+
+			var _cmd: string;
+			if (useSetblock)
+				_cmd = "execute @e[name={0},score_stdInteger=0] ~{1} ~-1 ~ setblock ~%X ~%Y ~%Z minecraft:redstone_block 0 replace".format(name, distance);
+			else
+				var _cmd = 'execute @e[name={0},score_stdInteger=0] ~{1} ~-1 ~ summon ArmorStand ~%X ~%Y ~%Z {CustomName:"call",NoGravity:true,Invincible:true,PersistenceRequired:true}'.format(name, distance);
+
 			outputHandler.addToCurrent(new Output.FunctionCall(id, _cmd));
 		}
 
